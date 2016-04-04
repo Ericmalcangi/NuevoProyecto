b6c95f3a6c7090ca6be18457e6c3d676aa2e10c8
Ericmalcangi <eric.malcangi@yahoo.com.ar>
2016-04-04 14:30:09 -0300

    Added files via upload

---
+++ NuevoProyecto/NuevaClase.java
@@ -0,0 +1,10 @@
+
+public class NuevaClase {
+	
+		public static void main(String[] args)	{
+			System.out.println("Nuevo proyecto");
+			System.out.println("Procedemos a copiar...");
+			System.out.println("... el proyecto");
+			
+		}
+}
